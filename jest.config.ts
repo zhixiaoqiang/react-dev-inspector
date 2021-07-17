a40f9b812c6b8c8ab348709f490286c34b701895
zthxxx <zthxxx.me@gmail.com>
2021-07-18 00:09:00 +0800

    chore: update config of jest / npm / ts

---
+++ jest.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from '@jest/types'
+
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testRegex: '\\.(test|spec)\\.(ts|tsx)$',
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/coverage/',
+    'fixtures',
+    '/dist/',
+    '/lib/',
+    '/es/',
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+  ],
+
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
+}
+
+export default config
