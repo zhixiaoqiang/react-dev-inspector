06b57fb29bf5fa872c3a958e32d799b734bb0fc0
zthxxx <zthxxx.me@gmail.com>
2021-08-27 21:10:00 +0800

    feat: inspect element immediately at mouse point while press hotkey

--- src/Inspector/Inspector.tsx
+++ src/Inspector/Inspector.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, useRef, ReactElement } from 'react'
+import React, { useEffect, useRef, ReactElement } from 'react'
 import type { Fiber } from 'react-reconciler'
 import hotkeys, { KeyHandler } from 'hotkeys-js'
-import { setupHighlighter } from './utils/hightlight'
+import { setupHighlighter } from './utils/highlight'
 import {
   getElementCodeInfo,
   gotoEditor,
@@ -50,10 +50,39 @@ export const Inspector: React.FC<InspectorProps> = (props) => {
     children,
   } = props
 
+  // hotkeys-js params need string
   const hotkey = (keys ?? defaultHotKeys).join('+')
 
-  const [isInspect, setIsInspect] = useState(false)
+  /** inspector tooltip overlay */
   const overlayRef = useRef<Overlay>()
+  const mousePointRef = useRef<{ x: number; y: number }>({ x: 0, y: 0 })
+
+  const recordMousePoint = ({ clientX, clientY }: MouseEvent) => {
+    mousePointRef.current.x = clientX
+    mousePointRef.current.y = clientY
+  }
+
+  const startInspect = () => {
+    const overlay = new Overlay()
+    overlayRef.current = overlay
+
+    const stopCallback = setupHighlighter({
+      onPointerOver: handleHoverElement,
+      onClick: handleClickElement,
+    })
+
+    overlay.setRemoveCallback(stopCallback)
+
+    // inspect element immediately at mouse point
+    const initPoint = mousePointRef.current
+    const initElement = document.elementFromPoint(initPoint.x, initPoint.y)
+    if (initElement) handleHoverElement(initElement as HTMLElement)
+  }
+
+  const stopInspect = () => {
+    overlayRef.current?.remove()
+    overlayRef.current = undefined
+  }
 
   const handleHoverElement = (element: HTMLElement) => {
     const overlay = overlayRef.current
@@ -74,13 +103,9 @@ export const Inspector: React.FC<InspectorProps> = (props) => {
   }
 
   const handleClickElement = (element: HTMLElement) => {
-    const overlay = overlayRef.current
-    overlay?.remove?.()
-    overlayRef.current = undefined
-    setIsInspect(false)
+    stopInspect()
 
     const codeInfo = getElementCodeInfo(element)
-
     const { fiber, name } = getElementInspect(element)
 
     if (!disableLaunchEditor) gotoEditor(codeInfo)
@@ -92,48 +117,40 @@ export const Inspector: React.FC<InspectorProps> = (props) => {
     })
   }
 
-  const startInspect = () => {
-    const overlay = new Overlay()
-
-    const stopCallback = setupHighlighter({
-      onPointerOver: handleHoverElement,
-      onClick: handleClickElement,
-    })
-
-    overlay.setRemoveCallback(stopCallback)
-
-    overlayRef.current = overlay
-    setIsInspect(true)
-  }
-
-  const stopInspect = () => {
-    overlayRef.current?.remove()
-    setIsInspect(false)
-  }
-
-  const handleInspectKey = () => (
-    isInspect
-      ? stopInspect()
-      : startInspect()
-  )
+  useEffect(() => {
+    document.addEventListener('mousemove', recordMousePoint, true)
+    return () => {
+      document.removeEventListener('mousemove', recordMousePoint, true)
+    }
+  }, [])
 
   useEffect(() => {
     const handleHotKeys: KeyHandler = (event, handler) => {
       if (handler.key === hotkey) {
-        handleInspectKey()
-      } else if (isInspect && handler.key === 'esc') {
+        overlayRef.current
+          ? stopInspect()
+          : startInspect()
+
+      } else if (handler.key === 'esc' && overlayRef.current) {
         stopInspect()
       }
     }
 
+    // https://github.com/jaywcjlove/hotkeys
     hotkeys(`${hotkey}, esc`, handleHotKeys)
-    window.__REACT_DEV_INSPECTOR_TOGGLE__ = handleInspectKey
+
+    /**
+     * @deprecated only for debug, will remove in next version
+     */
+    window.__REACT_DEV_INSPECTOR_TOGGLE__ = () => overlayRef.current
+      ? stopInspect()
+      : startInspect()
 
     return () => {
       hotkeys.unbind(`${hotkey}, esc`, handleHotKeys)
       delete window.__REACT_DEV_INSPECTOR_TOGGLE__
     }
-  }, [hotkey, isInspect, handleInspectKey])
+  }, [hotkey])
 
   return children as ReactElement
 }
